782735245c2384ec58d24769f1ee9a90377e9a70
Joonaynay <Joonaynay@gmail.com>
2021-11-08 16:29:37 -0700

    Pods

---
+++ Pods/Target Support Files/Pods-Kudu-KuduUITests/Pods-Kudu-KuduUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Kudu_KuduUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Kudu_KuduUITests
+@end
--- Pods/Target Support Files/Pods-StarFeed/Pods-StarFeed-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_StarFeed : NSObject
-@end
-@implementation PodsDummy_Pods_StarFeed
-@end
